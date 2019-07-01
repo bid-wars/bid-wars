3069683ec152f03b3c595a731e6fc146b9e16ec9
BigMike783 <45283126+BigMike783@users.noreply.github.com>
2019-07-01 10:59:38 -0600

    removed defaults and added redux store

---
+++ src/redux/ownerReducer.js
@@ -0,0 +1,55 @@
+const initialState = {
+    first_name: '',
+    last_name: '',
+    company_name: '',
+    website: '',
+    phone1: '',
+    phone2: '',
+    email:  '',
+    logo: '',
+    user_id: null,
+    bids: [],
+    schedule: []
+}
+
+    const UPDATE_USER = 'UPDATE_USER'
+
+    export function updateUser(user){
+        return{
+            type: UPDATE_USER,
+            payload: user
+        }
+    }
+
+function ownerReducer(state = initialState, action){
+    switch(action.type){
+        case UPDATE_USER:
+            const {first_name,
+            last_name,
+            company_name,
+            website,
+            phone1,
+            phone2,
+            email,
+            logo,
+            user_id,
+            bids,
+            schedule    
+            } = action.payload
+            return {first_name,
+                last_name,
+                company_name,
+                website,
+                phone1,
+                phone2,
+                email,
+                logo,
+                user_id,
+                bids,
+                schedule}
+        
+    }
+    return state
+}
+
+export default ownerReducer
