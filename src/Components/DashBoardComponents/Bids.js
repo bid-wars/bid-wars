94f171878bfa2bd4b3f624d4e9970f567266e734
BigMike783 <45283126+BigMike783@users.noreply.github.com>
2019-07-02 15:15:35 -0600

    added empty dashbord components with routes

---
+++ src/Components/DashBoardComponents/Bids.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Bids extends Component {
+    render() {
+        return (
+            <div>
+                <h1>bids</h1>
+            </div>
+        )
+    }
+}
---
+++ src/Components/DashBoardComponents/MainDash.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MainDash extends Component {
+    render() {
+        return (
+            <div>
+                <h1>main dash</h1>
+            </div>
+        )
+    }
+}
